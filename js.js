2dca74cb1954f3567acea01203fce2fae0710196
Zuzia <zuzannabrzeczek71@gmail.com>
2022-02-18 13:24:52 +0100

    web

---
+++ js.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("glowny");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
